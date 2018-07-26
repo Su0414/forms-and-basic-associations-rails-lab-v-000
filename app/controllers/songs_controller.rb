35c3b23c7af4f7dc344e13ef139b906818862b41
Suchitra Naik <susanaik@gmail.com>
2018-07-26 13:08:57 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -48,6 +48,6 @@ class SongsController < ApplicationController
 
   def song_params
     binding.pry
-    params.require(:song).permit(:title, :artist_name, :genre_id)
+    params.require(:song).permit(:title, :artist_name, :genre_id, :note_contents)
   end
 end
